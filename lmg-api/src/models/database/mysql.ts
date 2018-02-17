9c84cca53e82cdaecbde0d668f3d07dd987be891
Mats Andresen <mats.andresen@gmx.de>
2018-02-17 23:36:22 +0100

    Add basic mysql module

---
+++ lmg-api/src/models/database/mysql.ts
@@ -0,0 +1,42 @@
+import * as mysql from 'mysql'
+
+const MYSQL_HOSTNAME = process.env.MYSQL_HOSTNAME
+const MYSQL_USERNAME = process.env.MYSQL_USERNAME
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE
+
+export default class Mysql {
+    private connection: any
+
+    constructor() {
+        this.connect()
+    }
+
+    public connect(): void {
+        this.connection = mysql.createConnection({
+            host     : MYSQL_HOSTNAME,
+            user     : MYSQL_USERNAME,
+            password : MYSQL_PASSWORD,
+            database : MYSQL_DATABASE
+        })
+        this.connection.connect((error) => {
+            if(error) console.log(error)
+        })
+    }
+
+    public async query(statement: string, bindings: Array<any> = []): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.connection.query(statement, bindings, (error, results, fields) => {
+                if(error) {
+                    reject(error)
+                }
+
+                if(results.length > 1) {
+                    resolve(results)
+                } else {
+                    resolve(results[0])
+                }
+            })
+        })
+    }
+}
