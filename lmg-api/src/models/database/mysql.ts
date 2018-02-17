c19610485002c4c9498dac67ca0a8d179ac1fc42
Mats Andresen <mats.andresen@gmx.de>
2018-02-17 23:43:52 +0100

    Add basic entity modules

--- lmg-api/src/models/database/mysql.ts
+++ lmg-api/src/models/database/mysql.ts
@@ -5,7 +5,7 @@ const MYSQL_USERNAME = process.env.MYSQL_USERNAME
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE
 
-export default class Mysql {
+export class Mysql {
     private connection: any
 
     constructor() {
@@ -40,3 +40,5 @@ export default class Mysql {
         })
     }
 }
+
+export default new Mysql()
---
+++ lmg-api/src/models/entity/EntityRepository.ts
@@ -0,0 +1,6 @@
+import { Mysql } from '../database/mysql'
+import db from '../database/mysql'
+
+export default class EntityRepository {
+    protected db: Mysql
+}
